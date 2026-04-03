2639cf3c96f7642abb58e68b5fb676b230b0e34c
triet2203 <minhtriet2203@gmail.com>
2026-04-03 15:58:05 +0700

    nop bai tap

--- src/ss1_introduction_java/Currency.java
+++ src/ss1_introduction_java/Currency.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Currency {
     public static void main(String[] args) {
-        double vnd = 23000;
+        double exchange = 23000;
         double usd;
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập số tiền (USD): ");
         usd = sc.nextDouble();
-        double quydoi = usd * vnd;
-        System.out.println("Giá trị VND là " + quydoi);
+        double vndAmount = usd * exchange;
+        System.out.println("Giá trị VND là " + vndAmount + " VND");
     }
 }
